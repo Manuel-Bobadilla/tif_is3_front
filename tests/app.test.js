fbdd58903ea152c6e5af7cc9a9156ed684d022d4
Manuel-Bobadilla <manuelcba@outlook.com>
2022-12-12 12:12:22 -0300

    carga nuevos test

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,21 +1,45 @@
 import {JSDOM} from "jsdom"
 
 describe('Testing Html', () => {
-    test('Boton Mas', async () => {
+    test('Texto Boton Mas', async () => {
         const dom = await JSDOM.fromFile('/home/manuel/Documents/tif_front/index.html')
         const elementoHttp = dom.window.document.querySelector('#mas')
         expect(elementoHttp.textContent).toBe("+")
     })
 
-    test('Boton Menos', async () => {
+    test('Funcion Boton Mas', async () => {
+        const dom = await JSDOM.fromFile('/home/manuel/Documents/tif_front/index.html')
+        const elementoHttp = dom.window.document.querySelector('#mas')
+        expect(elementoHttp.getAttribute('onclick')).toBe("sumar()")
+    })
+
+    test('Texto Boton Menos', async () => {
         const dom = await JSDOM.fromFile('/home/manuel/Documents/tif_front/index.html')
         const elementoHttp = dom.window.document.querySelector('#menos')
         expect(elementoHttp.textContent).toBe("-")
     })
+
+    test('Funcion Boton Menos', async () => {
+        const dom = await JSDOM.fromFile('/home/manuel/Documents/tif_front/index.html')
+        const elementoHttp = dom.window.document.querySelector('#menos')
+        expect(elementoHttp.getAttribute('onclick')).toBe("restar()")
+    })
     
-    test('Campo Texto', async () => {
+    test('Texto Campo Texto', async () => {
         const dom = await JSDOM.fromFile('/home/manuel/Documents/tif_front/index.html')
         const elementoHttp = dom.window.document.querySelector('#texto')
         expect(elementoHttp.textContent).toBe("")
+    })
+
+    test('Solo Lectura Campo Texto', async () => {
+        const dom = await JSDOM.fromFile('/home/manuel/Documents/tif_front/index.html')
+        const elementoHttp = dom.window.document.querySelector('#texto')
+        expect(elementoHttp.getAttribute('readonly')).toBe("")
+    })
+
+    test('Solo Numeros Campo Texto', async () => {
+        const dom = await JSDOM.fromFile('/home/manuel/Documents/tif_front/index.html')
+        const elementoHttp = dom.window.document.querySelector('#texto')
+        expect(elementoHttp.getAttribute('type')).toBe("number")
     })
 })
